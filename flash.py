0265704b4782bacc84571c1f91dba105b60a2d06
guptaish <59252596+guptaish@users.noreply.github.com>
2019-12-26 16:57:14 +0530

    Add files via upload
    flash sale bot for amazon(modify program by adding your email,pass,url of deal and price)

---
+++ flash.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+browser=webdriver.Chrome()
+browser.get('https://www.amazon.in/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3Fref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&')#url of product
+browser.find_element_by_id('ap_email').send_keys('abc@gmail.com')#your amazon email
+browser.find_element_by_id('continue').click()
+browser.find_element_by_id('ap_password').send_keys('123456')#your password
+browser.find_element_by_id('signInSubmit').click()
+#i=0
+#n=str(input())
+#if(n=='true' or n=='T' or 0xFF==ord('o')):
+while(True):
+    browser.get('https://www.amazon.in/Fabulous-Shampoo-250ml-Conditioner-Reviver/dp/B00NCHR9BQ/')
+    price=browser.find_element_by_id('priceblock_ourprice')
+    #button=browser.find_element_by_id('buy-now-button')
+    cart=browser.find_element_by_id('add-to-cart-button')
+    deal=browser.find_element_by_id('priceblock_dealprice')        
+    p=price.text
+    d=deal.text
+    dealbutton=browser.find_element_by_id('a-autoid-0-announce')
+    if(d=='₹ 1,599.40'):#flash sale item price 
+        dealbutton.click()
+        break
+    
+
+
+    
+    
+    
